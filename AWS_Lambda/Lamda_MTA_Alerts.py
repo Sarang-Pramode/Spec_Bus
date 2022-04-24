d81c3a1f0bf3926da0150822917560bbaa72fbb2
Sarang-Pramode <sarangpramode1997@gmail.com>
2022-04-24 11:34:47 -0400

    error in lambda function, timeout after 3 seconds

--- AWS_Lambda/Lamda_MTA_Alerts.py
+++ AWS_Lambda/Lamda_MTA_Alerts.py
@@ -4,6 +4,7 @@ from google.transit import gtfs_realtime_pb2
 import pyarrow.parquet as pq
 import pyarrow as pa
 import pandas as pd
+import boto3
 
 # GET all routes with issues
 def getRouteWithAlerts(feed):
@@ -12,6 +13,14 @@ def getRouteWithAlerts(feed):
         for informed_entity in entity.alert.informed_entity:
             problem_routes.append(informed_entity.route_id)
     return problem_routes
+
+def Upload_to_s3(file_name):
+    s3_resource = boto3.resource('s3')
+    MY_BUCKET_NAME = 's3-script-tests6544a2ec-2be3-4d25-ab1e-1e3494d732c7'
+    s3_resource.Bucket(MY_BUCKET_NAME).upload_file(
+    Filename=file_name, Key=file_name)
+
+    return 0
 
 
 def lambda_handler(event, context):
@@ -37,10 +46,14 @@ def lambda_handler(event, context):
     # Write handler for failed responses
     feed.ParseFromString(response.content) #parse the response(protobuf) and store in feed  
 
-    problem_routes = getRouteWithAlerts(feed) 
+    return getRouteWithAlerts(feed) 
 
-    p_data = pd.DataFrame({'data':problem_routes})
-    table = pa.Table.from_pandas(p_data)
+    #p_data = pd.DataFrame({'data':problem_routes})
+    #table = pa.Table.from_pandas(p_data)
 
     #write to parqueet
-    pq.write_table(table,'example.parquet')
+    #pq.write_table(table,'AWS_Lambda/example.parquet')
+
+    #return print(Upload_to_s3('example.parquet'))
+
+    
